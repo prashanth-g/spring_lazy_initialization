fe36dbc8644addbe70a5b87b57306c0c1f921efe
prashanth-g <prashanthcsgce@gmail.com>
2019-03-14 18:18:45 +0530

    Added Lazy initialization

--- src/main/java/com/prashanth/spring/lazyinitialization/SpringLazyInitialization.java
+++ src/main/java/com/prashanth/spring/lazyinitialization/SpringLazyInitialization.java
@@ -1,4 +1,12 @@
 package com.prashanth.spring.lazyinitialization;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SpringLazyInitialization {
+    public static void main(String[] args) {
+        System.setProperty("spring.main.lazy-initialization", "true");
+        SpringApplication.run(SpringLazyInitialization.class, args);
+    }
 }
